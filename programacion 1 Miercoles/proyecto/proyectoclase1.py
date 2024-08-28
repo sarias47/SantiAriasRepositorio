969a9747adc829804fd592e7d630b587605fdcde
sarias47 <sarias@uade.edu.ar>
2024-08-28 14:19:44 -0300

    actualizo mis archivos

--- programacion 1 Miercoles/proyecto/proyectoclase1.py
+++
@@ -1,6 +0,0 @@
-estudiantes=["juan", "alberto", "martin", "federico", "tomas", "lucas"]
-materias=["Matematica", "Ingles", "Fisica", "Biologia", "Arte", "Musica", "Ed. Fisica"]
-matriz15X8=[]
-
-for fil in range(alumnos):
-    matriz15X8.append([0]*materias)
